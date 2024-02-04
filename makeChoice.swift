fdd316d9f0f464bca64d30a894c2adc129b58f5c
PBR <152909621+ruslangurskij@users.noreply.github.com>
2024-02-04 17:27:56 +0100

    Create makeChoice.swift

---
+++ makeChoice.swift
@@ -0,0 +1,8 @@
+int makeChoice() {
+    int choice;
+    std::cout << "Выберите действие:\n";
+    std::cout << "1. Войти в левую дверь.\n";
+    std::cout << "2. Войти в правую дверь.\n";
+    std::cin >> choice;
+    return choice;
+}
